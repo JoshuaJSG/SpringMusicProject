5e6689712862989961e76e4ff1ea5fd81e5f0440
Joshua Steele-Gomes <jsteelegomes@gmail.com>
2020-06-25 07:39:57 +0100

    rendering data from backend to show fields from  database

--- src/main/java/com/JSG/JSGApplication/myproject/src/App.js
+++ src/main/java/com/JSG/JSGApplication/myproject/src/App.js
@@ -6,16 +6,30 @@ import axios from "axios";
 
 
 const ArtistProfiles = () =>{
+const [artistProfiles, setArtistProfiles] = useState([]);
 
     const fetchArtists = () => {
         axios.get("http://localhost:8080/artists").then(res =>{
             console. log(res);
+            setArtistProfiles(res.data)
         });
     }
 
     useEffect(() => {
         fetchArtists();
     }, []);
+
+
+    return artistProfiles.map((artistProfile, index) =>{
+        return(
+            <div key={index}>
+                Name: {artistProfile.name}
+
+            </div>
+
+
+        )
+    })
 
     return <h1>Hello</h1>
     //extra [] when our list get updated, function is called again, updating our display to show incoming data.
