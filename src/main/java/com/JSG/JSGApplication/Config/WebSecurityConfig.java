80d0068671a4d87d8467ae1f543ea8e3beb3c2b4
Joshua Steele-Gomes <jsteelegomes@gmail.com>
2020-07-02 19:09:55 +0100

    Post working through react to add raw data to MySQL Database

--- src/main/java/com/JSG/JSGApplication/Config/WebSecurityConfig.java
+++
@@ -1,9 +0,0 @@
-package com.JSG.JSGApplication.Config;
-
-
-import org.springframework.boot.autoconfigure.security.servlet.WebSecurityEnablerConfiguration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-@EnableWebSecurity
-public class configFile extends WebSecurityEnablerConfiguration {
-}
--- src/main/java/com/JSG/JSGApplication/myproject/src/App.js
+++ src/main/java/com/JSG/JSGApplication/myproject/src/App.js
@@ -1,72 +1,113 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, Component } from 'react';
 import axios from "axios";
 
 
-const artistURL = "http://localhost:8080/artists"; 
+
+// const artistURL = "http://localhost:8080/artists"; 
+
+
+// const ArtistProfiles = () =>{
+// const [artistProfiles, setArtistProfiles] = useState([]);
+
+//     const fetchArtists = () => {
+//         axios.get(artistURL).then(res =>{
+//             console.log(res);
+//             setArtistProfiles(res.data)
+//         });
+//     }
+
+//     useEffect(() => {
+//         fetchArtists();
+//     }, []);
+
+
+//     return artistProfiles.map((artistProfile, index) =>{
+//         return(
+//             <div key={index}>
+//                 Name: {artistProfile.name}
+
+//             </div>
+
+
+//         )
+//     })
+//     //extra [] when our list get updated, function is called again, updating our display to show incoming data.
+// }
 
 
 
-const ArtistProfiles = () =>{
-const [artistProfiles, setArtistProfiles] = useState([]);
 
-    const fetchArtists = () => {
-        axios.get(artistURL).then(res =>{
-            console.log(res);
-            setArtistProfiles(res.data)
-        });
-    }
-
-    useEffect(() => {
-        fetchArtists();
-    }, []);
+// function App() {
 
 
-    return artistProfiles.map((artistProfile, index) =>{
-        return(
-            <div key={index}>
-                Name: {artistProfile.name}
-
-            </div>
-
-
-        )
-    })
-    //extra [] when our list get updated, function is called again, updating our display to show incoming data.
-}
+//     const artistNameRef = useRef() //accessing the values inside the textBox
 
 
 
-
-function App() {
-    const artistNameRef = useRef() //accessing the values inside the textBox
-
-
-
-    async function addingNewArtist(event){
-       event.preventDefault()
-       let artistName = artistNameRef.current.value
-        if (artistName === ' ') return
-        // artistName.current = null // clearing the text field after use.
-        try {
-            const response = await axios.post(artistURL, artistName);
-            console.log('Returned data:', response);
-          } catch (event) {
-            console.log(`Axios request failed: ${event}`);
-          }
-    }
+//     async function addingNewArtist(event){
+//        event.preventDefault()
+//        let artistName = artistNameRef.current.value
+//         if (artistName === ' ') return
+//         // artistName.current = null // clearing the text field after use.
+//         try {
+//             const response = await axios.post(artistURL, artistName);
+//             console.log('Returned data:', response);
+//           } catch (event) {
+//             console.log(`Axios request failed: ${event}`);
+//           }
+//     }
 
 
-    return(
-        <>
-            <ArtistProfiles/>
-            <input ref={artistNameRef} type="text" />
-            <button onClick={addingNewArtist}>Add Artist</button>
-            <button>Delete Artist</button>
+//     return(
+//         <>
+//             <ArtistProfiles/>
+//             <input ref={artistNameRef} type="text" />
+//             <button onClick={addingNewArtist}>Add Artist</button>
+//             <button>Delete Artist</button>
 
             
-        </>
-    );
+//         </>
+//     );
 
+// }
+
+const api = axios.create({
+    baseURL: "http://localhost:8080/artists"
+});
+
+
+class App extends Component{
+
+    state = {
+        artists: []
+    }
+
+        constructor(){
+        super();
+        api.get('/').then(res =>{
+            console.log(res.data)
+            this.setState({artists: res.data})
+        })
+    }
+
+
+    createArtist = async () =>{
+       let response = await api.post('/', {name: "miguel"})
+        console.log(response)
+    }
+
+
+    render(){
+        return(
+            <>
+            <button onClick={this.createArtist}>Add Artist</button>
+            {this.state.artists.map(artist => <h4 key={artist.id}>{artist.name}</h4>)}
+            </>
+        )
+        
+    }
 
 }
+
+
 export default App;
