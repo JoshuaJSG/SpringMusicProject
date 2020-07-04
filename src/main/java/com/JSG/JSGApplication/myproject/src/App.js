84a101531b72691f1caf17d530eb603a49d1a6cb
Joshua Steele-Gomes <jsteelegomes@gmail.com>
2020-07-04 18:48:10 +0100

    Persisting values to the database through text Input is now fully functionable

--- src/main/java/com/JSG/JSGApplication/myproject/src/App.js
+++ src/main/java/com/JSG/JSGApplication/myproject/src/App.js
@@ -1,76 +1,6 @@
 import React, { useState, useEffect, useRef, Component } from 'react';
 import axios from "axios";
 
-
-
-// const artistURL = "http://localhost:8080/artists"; 
-
-
-// const ArtistProfiles = () =>{
-// const [artistProfiles, setArtistProfiles] = useState([]);
-
-//     const fetchArtists = () => {
-//         axios.get(artistURL).then(res =>{
-//             console.log(res);
-//             setArtistProfiles(res.data)
-//         });
-//     }
-
-//     useEffect(() => {
-//         fetchArtists();
-//     }, []);
-
-
-//     return artistProfiles.map((artistProfile, index) =>{
-//         return(
-//             <div key={index}>
-//                 Name: {artistProfile.name}
-
-//             </div>
-
-
-//         )
-//     })
-//     //extra [] when our list get updated, function is called again, updating our display to show incoming data.
-// }
-
-
-
-
-// function App() {
-
-
-//     const artistNameRef = useRef() //accessing the values inside the textBox
-
-
-
-//     async function addingNewArtist(event){
-//        event.preventDefault()
-//        let artistName = artistNameRef.current.value
-//         if (artistName === ' ') return
-//         // artistName.current = null // clearing the text field after use.
-//         try {
-//             const response = await axios.post(artistURL, artistName);
-//             console.log('Returned data:', response);
-//           } catch (event) {
-//             console.log(`Axios request failed: ${event}`);
-//           }
-//     }
-
-
-//     return(
-//         <>
-//             <ArtistProfiles/>
-//             <input ref={artistNameRef} type="text" />
-//             <button onClick={addingNewArtist}>Add Artist</button>
-//             <button>Delete Artist</button>
-
-            
-//         </>
-//     );
-
-// }
-
 const api = axios.create({
     baseURL: "http://localhost:8080/artists"
 });
@@ -78,30 +8,61 @@ const api = axios.create({
 
 class App extends Component{
 
-    state = {
-        artists: []
-    }
 
-        constructor(){
-        super();
-        api.get('/').then(res =>{
-            console.log(res.data)
-            this.setState({artists: res.data})
-        })
+    state = {
+        artists: [],
+        theArtistName: '',
+      };
+
+    constructor(props) {
+        super(props);
+        this.handleArtistNameChange = this.handleArtistNameChange.bind(this);
+       
+      }
+
+
+    //calling this method will allow artist array to be populated everytime an event occurs, e.g POST, PUT, DELETE
+    getArtists = async () =>{
+        let data = await api.get("/").then(({ data }) => data);
+        this.setState({artists: data}) //setting our artists to be the data we fetch
     }
 
 
     createArtist = async () =>{
-       let response = await api.post('/', {name: "miguel"})
-        console.log(response)
+       let response = await api.post('/', {name: this.state.theArtistName})
+       this.setState({theArtistName: ""})
+       this.getArtists()
     }
 
 
+    deleteArtist = async (id) =>{
+        let data = await api.delete('/${id}')
+        this.getArtists();
+    }
+
+   
+      
+
+    componentDidMount(){
+        this.createArtist()
+
+    }
+
+    //This is called everytime the textbox is updated
+    handleArtistNameChange(event) {
+        this.setState({ theArtistName: event.target.value });
+      }
+
     render(){
         return(
-            <>
-            <button onClick={this.createArtist}>Add Artist</button>
-            {this.state.artists.map(artist => <h4 key={artist.id}>{artist.name}</h4>)}
+            <>           
+            <input type='Text' placeholder="Enter Artist Name" 
+            name="theArtistName" 
+            value={this.state.theArtistName}
+            onChange={this.handleArtistNameChange}></input>
+                <button onClick={this.createArtist}>Add Artist</button>
+                    {this.state.artists.map(artist => <h4 key={artist.id}>{artist.name}
+                <button onClick={() =>this.deleteArtist(artist.id)}>Delete artist</button></h4>)}
             </>
         )
         
