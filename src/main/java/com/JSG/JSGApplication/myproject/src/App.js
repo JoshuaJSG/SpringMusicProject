a0ba38eed501d3aaaaea6086d6447a52e1b133da
Joshua Steele-Gomes <jsteelegomes@gmail.com>
2020-07-09 19:19:42 +0100

    background for entire artist component

--- src/main/java/com/JSG/JSGApplication/myproject/src/App.js
+++ src/main/java/com/JSG/JSGApplication/myproject/src/App.js
@@ -10,9 +10,15 @@ const stripStyle = {
     padding: '1.5rem'
 }
 
+const artistStrip = {
+    background: '#fab1a0',
+    padding: '1.5rem'
+}
+
+//adding a space after the header backround
 const emptySpace = {
     background: 'white',
-    padding: '1rem'
+    padding: '0.5rem'
 }
 
 
@@ -30,84 +36,84 @@ const api = axios.create({
 
 
 
-class App extends Component{
+class App extends Component {
 
 
     state = {
         artists: [],
         theArtistName: '',
-      };
+    };
 
     constructor(props) {
         super(props);
         this.handleArtistNameChange = this.handleArtistNameChange.bind(this);
-       
-      }
+
+    }
 
 
     //calling this method will allow artist array to be populated everytime an event occurs, e.g POST, PUT, DELETE
-    getArtists = async () =>{
+    getArtists = async () => {
         let data = await api.get("/").then(({ data }) => data);
-        this.setState({artists: data}) //setting our artists to be the data we fetch
+        this.setState({ artists: data }) //setting our artists to be the data we fetch
     }
 
 
-    createArtist = async () =>{
+    createArtist = async () => {
         this.getArtists()
-        if (!this.state.theArtistName == ""){
-            let response = await api.post('/', {name: this.state.theArtistName})
-            this.setState({theArtistName: ""})
-        }return
+        if (!this.state.theArtistName == "") {
+            let response = await api.post('/', { name: this.state.theArtistName })
+            this.setState({ theArtistName: "" })
+        } return
     }
 
 
-    deleteArtist = async (id) =>{
+    deleteArtist = async (id) => {
         let data = await api.delete('/${id}')
         this.getArtists();
     }
 
-   
-      
+
+
 
     //Once data is changed it is "Mounted" 
-    componentDidMount(){
+    componentDidMount() {
         document.title = "Artist Song Viewer"
         this.createArtist()
 
     }
 
-    componentWillUpdate(){
+    componentWillUpdate() {
         this.getArtists()
     }
     //This is called everytime the textbox is updated
     handleArtistNameChange(event) {
         this.setState({ theArtistName: event.target.value });
-      }
+    }
 
-    render(){
-        return(
+    render() {
+        return (
             <>
-            <ThemeProvider theme={theme}>
-            <div className="App">
+                <ThemeProvider theme={theme}>
+                    <div className="App">
+                        <div style={stripStyle}>
+                            <H1>Artist Retrieval App</H1>
+                            <input type='Text' placeholder="Enter Artist Name" name="theArtistName"
+                                value={this.state.theArtistName}
+                                onChange={this.handleArtistNameChange}>
+                            </input>
+                            <Button onClick={this.createArtist}>Add Artist</Button>
 
-                
-                <div style={stripStyle}>
-                <H1>Artist Retrieval App</H1>
-                <input type='Text' placeholder="Enter Artist Name" name="theArtistName" 
-                    value={this.state.theArtistName}
-                    onChange={this.handleArtistNameChange}>
-                </input>
-                <Button onClick={this.createArtist}>Add Artist</Button>
-                
-                </div>
-                <div style ={emptySpace}></div>
-                {this.state.artists.map(artist => <h4 key={artist.id}>{artist.name}
-                <Button onClick={() =>this.deleteArtist(artist.id)}>Delete artist</Button></h4>)}
-            </div>
-            </ThemeProvider>
+                        </div>
+                        <div style={emptySpace}></div>
+                        <div style={artistStrip}>
+                            {this.state.artists.map(artist => <h4 key={artist.id}>{artist.name}
+                                <Button onClick={() => this.deleteArtist(artist.id)}>Delete artist</Button></h4>)}
+                        </div>
+                    </div>
+                </ThemeProvider>
             </>
         )
-        
+
     }
 
 }
